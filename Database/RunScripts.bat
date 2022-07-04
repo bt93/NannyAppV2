db84e2b4fbb245e41b73eee7d2d042329d4cf1b3
Jason Howie <howiejason93@gmail.com>
2022-07-04 08:23:12 -0400

    Add batch file to run all SQL scripts

---
+++ Database/RunScripts.bat
@@ -0,0 +1,4 @@
+SET SQLCMD=sqlcmd -S .\SQLEXPRESS -d NannyDB
+for %%d in (*.sql) do %SQLCMD% -i%%d
+for %%d in (.\DataScripts\*.sql) do %SQLCMD% -i%%d
+for %%d in (.\StoredProcedures\*.sql) do %SQLCMD% -i%%d
