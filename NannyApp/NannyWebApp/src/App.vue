ba9c494af1272edc63ee569ae40e5e7493d36a18
Jason Howie <howiejason93@gmail.com>
2022-07-24 10:29:00 -0400

    updates

--- NannyApp/NannyWebApp/src/App.vue
+++ NannyApp/NannyWebApp/src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import NavBar from './components/templates/NavBar.vue'
+
 </script>
 
 <template>
+  <NavBar />
   <RouterView />
 </template>
 
---
+++ NannyApp/NannyWebApp/src/components/templates/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav class="relative container mx-auto p-6">
+        <div class="flex items-center justify-between">
+            <div class="pt-2 w-36">
+                <img src="@/assets/images/logo.png" alt="">
+            </div>
+        </div>
+    </nav>
+</template>
